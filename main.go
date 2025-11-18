9bdb69ea162f6d87129f6001d0a04325dcf16b38
Ethan White <ethan.white1@stu.ocsb.ca>
2025-11-18 19:50:25 +0000

    Copied code from image

---
+++ main.go
@@ -0,0 +1,45 @@
+// Author: Ethan White
+// Version: 1.0.0
+// Date: 2025-11-18
+// Fileoverview: This program asks the user for their name and age
+//               and then returns that information.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// variables
+	var userName string
+	var ageAsString string
+	var ageAsNumber int
+	var ageFiveYearsAgo int
+
+	reader := bufio.NewReader(os.Stdin)
+
+	// input
+	fmt.Print("What is your name? ")
+	userName, _ = reader.ReadString('\n')
+	userName = strings.TrimSpace(userName)
+
+	fmt.Print("How old are you? ")
+	ageAsString, _ = reader.ReadString('\n')
+	ageAsString = strings.TrimSpace(ageAsString)
+
+	// process
+	ageAsNumber, _ = strconv.Atoi(ageAsString)
+	ageFiveYearsAgo = ageAsNumber - 5
+
+	// output
+	fmt.Println()
+	fmt.Printf("Hello, " + userName + "!\n")
+	fmt.Printf("Five years ago, you were " + strconv.Itoa(ageFiveYearsAgo) + " years old.\n")
+
+	fmt.Println("\nDone.")
+}
